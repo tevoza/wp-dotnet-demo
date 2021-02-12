23493770b3ce412865615d3a36b63936c7ff85f2
Adam Coetzee <adam.coetzee@gmail.com>
2021-02-13 00:51:07 +0200

    testing

--- Plugins/DemoWidget.cs
+++ Plugins/DemoWidget.cs
@@ -3,24 +3,34 @@ using Pchp.Core.Reflection;
 using PeachPied.WordPress.Standard;
 using PeachPied.WordPress;
 using PeachPied.WordPress.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Peachpie.AspNetCore.Mvc;
 using Peachpie.AspNetCore.Web;
 using Peachpie;
+using System;
+using JokesWebApp.Models;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace JokesWebApp.Plugins
 {
     public class DemoWidget : WP_Widget
     {
         Context ct;
+        public IEnumerable<JokesWebApp.Models.Joke> JokeList { get; set; }
+
         public DemoWidget(Context ctx) : base(
             "demo_widget",
             "Demo Widget",
             new PhpArray(2) { { "classname", "DemoWidget" }, { "description", "Wordpress widget in dotnet" } },
-            "yeet"
+            "idk"
         )
         {
             this.ct = ctx;
+            //get a list of jokes objects from JokesController?
+            //JokeList = await
         }
 
         public string Title { get; } = "Demo Widget";
@@ -30,6 +40,7 @@ namespace JokesWebApp.Plugins
             WP_Hook Hook = new WP_Hook();
             PhpValue title = Hook.apply_filters("widget_title", instance["title"]);
             ct.RenderPartial("DemoWidget", this);
+            //return RedirectResult("/Home/Privacy");
             
             return title;
         }
